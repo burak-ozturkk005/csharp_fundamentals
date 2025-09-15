6b88a3d09bd1382bb769b48e52b4f4bd02de0f7d
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-15 12:47:45 +0300

    stringbuilder2

--- c01_Strings/string_builder/builder02/builder02/Program.cs
+++ c01_Strings/string_builder/builder02/builder02/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿//Kapasite
+using System.Text;
+
+StringBuilder sb = new StringBuilder(50); // Başlangıç kapasitesi 50
+sb.Append("Merhaba");
+Console.WriteLine(sb.Length); // 7
+//İkisi farklı şeyler
+sb.Insert(0, "Selam "); //Başa ekler
+sb.Replace("Selam", "Burak"); //değiştirme
+sb.Remove(0, 6); // ilk 6 karakteri siler
+sb.Clear(); // Tamamen sıfırlar.
+//Karakterlere erişim  normal  arraylerde olduğu gibidir.
+StringBuilder sbc = new StringBuilder("TEst");
+Console.WriteLine(sbc[0]);
+sbc[0] = 'B';
+Console.WriteLine(sbc);
+//Gorev 2 Çarpım Tablosu
+StringBuilder carpimTablosu = new StringBuilder();
+for(int i = 1; i <= 10; i++)
+{
+    for(int j = 1; j <= 10; j++)
+    {
+        carpimTablosu.Append($" {i} x {j} = {i * j}");
+        if(j < 10)
+        {
+            carpimTablosu.Append(" | ");
+        }
+    }
+    carpimTablosu.AppendLine(); //satır sonu alt satıra geç
+}
+Console.WriteLine(carpimTablosu.ToString());
--- c01_Strings/string_builder/builder02/builder02/obj/Debug/net9.0/builder02.AssemblyInfo.cs
+++ c01_Strings/string_builder/builder02/builder02/obj/Debug/net9.0/builder02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("builder02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3a01ad6c66b91e0bc172949e2e0ed529987ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02252ae09584cc728132533bcac7e334dc457d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("builder02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("builder02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
