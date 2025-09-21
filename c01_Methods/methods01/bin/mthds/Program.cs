85da2a39d044a43d5c5a626f6cf73477a8ed9a6e
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-21 12:42:32 +0300

    mthds

---
+++ c01_Methods/methods01/bin/mthds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace mthds
+{
+    internal class Program
+    {
+        internal static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+        internal static void SelamVer()
+        {
+            Console.WriteLine("Merhaba Burak!");
+        }
+        public static void Selam(string isim)
+        {
+            Console.WriteLine($"Merhaba {isim}!");
+        }
+        public static int Topla(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * erişim_belirleyici geri_donus_tipi MetotAdi(parametreler)
+             * {
+             * kodlar
+             * return deger; (geri donus varsa)
+             * }
+             * */
+            Console.WriteLine(Topla(4,5)); // ayriyeten geriye deger dondurur.
+            /*Geriye deger dondurmeyen void
+             *
+             */
+            SelamVer();
+            //Parametre Alan
+            Selam("Ayşe");
+            //Method Overloading (aşırı yükleme
+            //Aynı isimli fakat farklı parametreli metodlar.
+            Console.WriteLine(Topla(4, 5, 6));
+        }
+    }
+}
