8f97ab59d444ab859b778183bc79ed79c108b092
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-21 15:12:44 +0300

    boxng

---
+++ c00/boxing_unboxing/box/Program.cs
@@ -0,0 +1,14 @@
+﻿//BOXING UNBOXING
+//kutulama (boxing)
+//Bir deger tipini, object veya interface türünde referans tipine dönüştürme işlemidir.
+
+int i = 56;
+object o = i;
+
+//kutudan çıkarma - unboxing
+//Daha önce box edilen bir değeri tekrar kendi değer tipine çıkartma işlemidir.
+//cast kullanılır.
+i *= 2;
+i = (int)o;
+Console.WriteLine("Deger turu: {0}",i); //56
+Console.WriteLine("Referans turu: {0}",o); //56
---
+++ c00/const/New folder/sabit/Program.cs
@@ -0,0 +1,7 @@
+﻿//Değiştirilemez sabit değerler tanımlamak için kullanılır.
+//Derleme(compile time) sırasında değeri bilinmek zorundadır.
+//static const beraber kullanılmaz!
+//static const int c = 23;
+//mutlaka ilk değer ataması yapılmalıdır.
+const int c1 = 3;
+Console.WriteLine(c1);
