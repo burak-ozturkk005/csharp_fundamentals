54d2e1b48d47e98fdd1648a62419acd62415be4d
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-24 10:48:29 +0300

    prc

---
+++ OOP/practice/prac2/prc2/Ogrenci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prc2
+{
+    internal class Ogrenci
+    {
+        private int _not;
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Not
+        {
+            get { return _not; }
+            set
+            {
+                if(value >= 0 && value <= 100)
+                {
+                    _not = value;
+                }else
+                {
+                    Console.WriteLine("Hata: Not 0-100 arasında olmalı!");
+                }
+            }
+        }
+        public void NotGoster()
+        {
+            Console.WriteLine($"{Ad} {Soyad}  ogrencinin notu {_not}");
+        }
+
+    }
+}
---
+++ OOP/practice/prac2/prc2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace prc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Ad = "Burak";
+            ogr.Soyad = "Jason";
+            ogr.Not = 75;
+            ogr.NotGoster();
+        }
+    }
+}
